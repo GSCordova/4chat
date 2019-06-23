ce62c04615d101bcfc2ae2b37f35962546f79b1f
gianmarco <gianmarco0747@gmail.com>
2019-06-23 03:50:30 +0200

    added styles

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,13 +4,15 @@ import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 
 const Material = [
     MatToolbarModule,
     MatButtonModule,
     MatCheckboxModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatMenuModule
 ];
 
 @NgModule({
